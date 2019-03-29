278dba3ccfa5a14113999d83a207235cc0689655
randallale <randallale@urbandaleschools.com>
2019-03-29 10:46:00 -0500

    Testing

---
+++ Test.py
@@ -0,0 +1,14 @@
+class Character():
+    def __int__(self):
+        self.name = ""
+        self.status = ""
+        self.strength = 0
+        self.midichlorian_count = 0
+        self.home = ""
+def create_character:
+    jedi_1 = Character()
+    jedi_1.name ="Alex"
+    jedi_1.status = "Initiate"
+    jedi_1.strength = 50
+    jedi_1.midichlorian_count = 20
+    jedi_1.home = "Urbandale"
