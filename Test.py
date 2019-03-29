687fc19d566d7239488b0c988dec858d0a3f1966
randallale <randallale@urbandaleschools.com>
2019-03-29 12:18:13 -0500

    Testing

--- Test.py
+++ Test.py
@@ -5,10 +5,13 @@ class Character():
         self.strength = 0
         self.midichlorian_count = 0
         self.home = ""
-def create_character:
-    jedi_1 = Character()
-    jedi_1.name ="Alex"
-    jedi_1.status = "Initiate"
-    jedi_1.strength = 50
-    jedi_1.midichlorian_count = 20
-    jedi_1.home = "Urbandale"
+    def fight(self):
+        print("Hi")
+jedi_1 = Character()
+jedi_1.name ="Alex"
+jedi_1.status = "Initiate"
+jedi_1.strength = 50
+jedi_1.midichlorian_count = 20
+jedi_1.home = "Urbandale"
+
+jedi_1.fight()
