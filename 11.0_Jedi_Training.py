08056719903ccf216b062145d85982e3cb36bcb0
hmarmal <hmarmal@urbandaleschools.com>
2020-02-20 13:56:56 -0600

    jedi11

--- 11.0_Jedi_Training.py
+++ 11.0_Jedi_Training.py
@@ -1,33 +1,76 @@
 '''
-Sign your name:________________
+Sign your name:Malsawmthara Hmar
  
  1. Write code that defines a class named Animal:
      * Add a constructor for the Animal class that prints 'An animal has been born.'
      * Add an attribute for the animal name.
      * Add an eat() method for Animal that prints 'Munch munch.'
      * Add a make_noise() method for Animal that prints 'Grrr says [animal name].'
+'''
+class Animal():
+    def __init__(self,name):
+        print("An animal has been born.")
+        self.name=name
+    def eat(self):
+        print("Munch munch")
+    def make_noise(self):
+        print("Grr says", self.name +".")
 
-     
+'''     
  2. Write code that defines a class named Cat:
      * Make Animal the parent.
      * Add a constructor for Cat that prints 'A cat has been born.'
      * Modify the constructor so it calls the parent constructor as well.
      * Add a make_noise() method for Cat that prints 'Meow says [animal name].'
-
-     
+'''
+class Cat(Animal):
+    def __init__(self,name):
+        super().__init__(name)
+        print("A cat has been born.")
+        super().eat()
+        self.animal="cat"
+    def make_noise(self):
+        print("Meow says",self.animal ,self.name +".")
+'''
  3. Write code that defines a class named Dog:
      
      * Make Animal the parent.
      * Add a constructor for Dog that prints 'A dog has been born.'
      * Modify the constructor so it calls the parent constructor as well.
      * Add a make_noise() method for Dog that prints 'Bark says [animal name].'
+'''
 
-     
+class Dog(Animal):
+    def __init__(self,name):
+        super().__init__(name)
+        print("A dog has been born")
+        super().eat()
+        self.animal="dog"
+    def make_noise(self):
+        print("Bark says ",self.animal,self.name +".")
+
+
+'''
  4. Write a main program with:
      
      * Code that creates a cat, two dogs, and an animal with names.
      * Code that calls eat() and make_noise() for each animal.
-     
+'''
+def main():
+    ani=Cat("Gorgy")
+    ani.make_noise()
+    anii=Dog("Fishy")
+    anii.make_noise()
+    aniii = Dog("Floppy")
+    aniii.make_noise()
+    an=Animal("POP")
+    an.eat()
+    an.make_noise()
+
+
+if __name__=="__main__":
+    main()
+'''
  OUTPUT:
 An animal has been born.
 A cat has been born.
