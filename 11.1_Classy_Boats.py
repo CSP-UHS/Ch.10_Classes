9b237156c7d13db4e768083c80d2e2824460ffd7
Flare Flare Flare <flyrmat@urbandaleschools.com>
2022-02-04 13:36:00 -0600

    done1

--- 11.1_Classy_Boats.py
+++ 11.1_Classy_Boats.py
@@ -1,39 +1,39 @@
-'''
-CLASSY BOATS
------------------
-Use the following Pseudocode to create this program:
+class Boat():
+    def __init__(self, name):
+        self.name = name
+        self.isDocked = True
+    def dock(self):
+        if self.isDocked == True:
+            print(self.name,"is already docked")
+        else:
+            print(self.name, "is docking")
+            self.isDocked = True
+    def undock(self):
+        if self.isDocked == False:
+            print(self.name,"is already undocked")
+        else:
+            print(self.name,"is undocking")
+            self.isDocked = False
 
-1.) Create a class called Boat( )
-2.) Use a constructor that sets two attributes: name and isDocked.
-3.) The name should be entered as an argument when the object is created.
-4.) The isDocked attribute is a Boolean variable. Set it to True.
-5.) Add a method called dock( )
-6.) In this method, if the boat is already docked print "(boat name) is already docked."
-7.) If it is not docked, print "(boat name) is docking" and set the isDocked attribute to True.
-8.) Add another method called undock( )
-9.) In this method, if the boat is already undocked print "(boat name) is already undocked."
-10.) If it is docked print "(boat name) is undocking" and set the isDocked attribute to False.
-11.) Add another class called Submarine( ) that will inherit the Boat( ) class.
-12.) In the Submarine( ) class create a method called submerge( ) that will print "(boat name) is submerging" 
-if the boat is undocked and "(boat name) can't submerge" if the boat is docked.
+class Submarine(Boat):
+    def submerge(self):
+        if self.isDocked == True:
+            print(self.name,"can't submerge.")
+        else:
+            print(self.name,"is submerging")
+            self.isDocked = True
 
-Let's Float the Boat
---------------------
-13.) Instantiate an object of the Submarine( ) class. Don't forget to pass in a name.
-14.) Call the dock( ) method once
-15.) Call the undock( ) method twice
-16.) Call the dock( ) method two more times
-17.) Call the submerge( ) method once
-18.) Call the undock( ) method once
-19.) Call the submerge( ) method a final time.
+def my_program():
+    boat1 = Submarine("SS Georgie")
+    boat1.dock()
+    boat1.undock()
+    boat1.undock()
+    boat1.dock()
+    boat1.dock()
+    boat1.submerge()
+    boat1.undock()
+    boat1.submerge()
 
-OUTPUT:
-USS Hermon is already docked.
-USS Hermon is undocking
-USS Hermon is already undocked.
-USS Hermon is docking
-USS Hermon is already docked.
-USS Hermon can't submerge!
-USS Hermon is undocking
-USS Hermon is submerging!
-'''
+if __name__ == '__main__':
+    my_program()
+
--- 11.2_1000_Circles.py
+++ 11.2_1000_Circles.py
@@ -1,10 +1,27 @@
-'''
-Open an arcade window that is 500 x 300 pixels and named 1000 Circles.
-Create a class called Circle.
-Initialize the x and y positions to be randomly placed somewhere in the window.
-Initialize the radius to be 10 pixels.
-Initialize the color to randomized 0-255 RGB color format.
-Add a method to the Circle Class called draw_circle and draw the circle.
-In the main program, use a for loop to call the Circle class and draw it 1000 times.
-Feel free to see what happens if you draw it 10,000 times as well.
-'''
+import arcade
+import random
+radius = 10
+graph_x = 500
+graph_y = 300
+class Circle():
+    def __init__(self):
+        self.x = random.randint(radius,graph_x-radius)
+        self.y = random.randint(radius,graph_y-radius)
+        self.radius = radius
+        self.color1 = random.randint(0,255)
+        self.color2 = random.randint(0, 255)
+        self.color3 = random.randint(0, 255)
+    def draw_circle(self):
+        arcade.draw_circle_filled(self.x,self.y,self.radius,[self.color1, self.color2, self.color3])
+
+def my_program():
+    arcade.open_window(graph_x, graph_y, "1000 Circles", True)
+    arcade.set_background_color(arcade.color.WHITE)
+    arcade.start_render()
+    for i in range(1000):
+        circle = Circle()
+        circle.draw_circle()
+    arcade.finish_render()
+    arcade.run()
+if __name__ == '__main__':
+    my_program()
