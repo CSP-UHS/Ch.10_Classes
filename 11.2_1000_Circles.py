c21558b0a32f421804715c791deb733c47adf157
fuchswil <fuchswil@urbandaleschools.com>
2022-02-04 13:34:21 -0600

    BB8 and Classes

--- 11.2_1000_Circles.py
+++ 11.2_1000_Circles.py
@@ -8,3 +8,34 @@ Add a method to the Circle Class called draw_circle and draw the circle.
 In the main program, use a for loop to call the Circle class and draw it 1000 times.
 Feel free to see what happens if you draw it 10,000 times as well.
 '''
+import arcade
+import random
+sw = 1000
+sh = 700
+class Circle():
+    def __init__(self):
+        self.r = random.randint(5, 110)
+        self.x = random.randint(self.r, sw-self.r)
+        self.y = random.randint(self.r, sh-self.r)
+        self.clr = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+    def draw_circle(self):
+        arcade.draw_circle_outline(self.x, self.y, self.r, self.clr, 5)
+
+def myprogram():
+    arcade.open_window(sw, sh, "1000 Circles")
+    arcade.set_background_color(arcade.color.WHITE)
+    arcade.start_render()
+
+    for i in range(1000):
+        c = Circle()
+        c.draw_circle()
+
+
+
+
+    arcade.finish_render()
+    arcade.run()
+
+if __name__ == "__main__":
+    myprogram()
